96a69ab1f0bdf25e31cf5b024e27034092273671
Rafael Lima <contato@rafael.adm.br>
2013-01-19 17:40:45 -0200

    fix initializer example

--- examples/config_initializers_cobregratis.rb
+++ examples/config_initializers_cobregratis.rb
@@ -1,11 +1,11 @@
 if Rails.env != 'test' then
-  Cobregratis::Base.site = 'https://example.com.i'
+  Cobregratis::Base.site = 'https://app.cobregratis.com.br'
   # For backward compatability
   if Cobregratis::Base.respond_to? :user
-    Cobregratis::Base.user = 'my_fancy_username'
-    Cobregratis::Base.password = 'my_fancy_password'
+    Cobregratis::Base.user = 'my_token'
+    Cobregratis::Base.password = 'X'
   else 
-    Cobregratis::Base.site = 'https://my_fancy_username:my_fancy_password@example.com.i'
+    Cobregratis::Base.site = 'https://my_token:X@app.cobregratis.com.br'
   end
   # The cache store can be anything that ActiveSupport can handle
   Cobregratis::Base.connection.cache_store = ActiveSupport::Cache.lookup_store :mem_cache_store
