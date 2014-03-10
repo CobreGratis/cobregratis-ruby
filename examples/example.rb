6c19847294b328a4b4eef4c5459cde506d187a23
Rafael Lima <contato@rafael.adm.br>
2014-03-10 12:37:03 -0300

    fix example

--- examples/example.rb
+++ examples/example.rb
@@ -1,8 +1,9 @@
-require 'lib/cobregratis'
+$: << File.expand_path('../../lib', __FILE__)
+require 'cobregratis'
 require 'pp'
 
 Cobregratis::Base.site = 'https://app.cobregratis.com.br'
-Cobregratis::Base.user = 'ddxm00pJnGhrYJhyypUgpwqkbvKMASEznqRM2MANv6nqz08APfWxdurtUJoz'
+Cobregratis::Base.user = 'token-do-usuario'
 Cobregratis::Base.password = 'X'
 
 @bank_billet = Cobregratis::BankBillet.create({
@@ -11,14 +12,13 @@ Cobregratis::Base.password = 'X'
   :name => 'Rafael Lima',
 })
 
-sleep(1)
-@billets = Cobregratis::BankBillet.find(:all)
+@bank_billets = Cobregratis::BankBillet.find(:all)
 
-@billets.each do |bank_billet|
-  puts "Nosso Número: #{@bank_billet.our_number}\n";
-  puts "Vencimento: #{@bank_billet.expire_at}\n";
-  puts "Valor: #{@bank_billet.amount}\n";
-  puts "Sacado: #{@bank_billet.name}\n";
-  puts "URL: #{@bank_billet.external_link}\n";
+@bank_billets.each do |bank_billet|
+  puts "Nosso Número: #{bank_billet.our_number}\n";
+  puts "Vencimento: #{bank_billet.expire_at}\n";
+  puts "Valor: #{bank_billet.amount}\n";
+  puts "Sacado: #{bank_billet.name}\n";
+  puts "URL: #{bank_billet.external_link}\n";
   puts "=================================\n";
 end
