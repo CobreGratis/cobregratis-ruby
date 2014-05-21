aca0658e32ff80437c7c6f3cefb286036e5e2be1
Kivanio Barbosa <kivanio@gmail.com>
2014-05-21 10:26:15 -0400

    wrap all parameters in root element
    * fix issue with parameters be outside root when do request

--- lib/cobregratis/base.rb
+++ lib/cobregratis/base.rb
@@ -1,5 +1,6 @@
 module Cobregratis
   class Base < ActiveResource::Base
-    self.site = "https://app.cobregratis.com.br"
+    self.site = 'https://app.cobregratis.com.br'
+    self.include_root_in_json = true
   end
 end
