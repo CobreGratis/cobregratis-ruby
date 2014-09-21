ceed5d603972ea8ec89e6aadc5bbd1c3589b14ac
Kivanio Barbosa <kivanio@gmail.com>
2014-09-20 20:03:54 -0400

    update labels

--- examples/example.rb
+++ examples/example.rb
@@ -2,27 +2,30 @@ $: << File.expand_path('../../lib', __FILE__)
 require 'cobregratis'
 require 'pp'
 
-# Cobregratis::Base.site = 'http://staging.cobregratis.com.br'
-Cobregratis::Base.user = 'uiF3x9JuMFMBMasanVPo'
+Cobregratis::Base.site = 'https://app.cobregratis.com.br'
+Cobregratis::Base.user = 'minha_chave'
 Cobregratis::Base.password = 'X'
  
 
 # Criando um boleto
-# bank_billet = Cobregratis::BankBillet.create({
-#   :name => 'Cliente',
-#   :cnpj_cpf => '34458851179',
-#   :amount => 13.50,
-#   :expire_at => '2015-07-30',
-#   :parcels => 1, # Número de parcelas
-#   :generate_on_creation => true,
-#   :save_customer => true, # salvar cliente
-#   :service_id => '46270',
-#   email: { # para onde o boleto será enviado
-#          name: 'Cliente',
-#          address: 'cliente@example.com.br'
-#        }
-# })
-# pp bank_billet
+bank_billet = Cobregratis::BankBillet.create({
+  :name => 'Cliente',
+  :cnpj_cpf => '34458851179',
+  :amount => 13.50,
+  :expire_at => '2015-08-30',
+  :parcels => 1, # Número de parcelas
+  :generate_on_creation => true,
+  :bank_billet_account_id => 11098,
+  # :save_customer => true, # salvar cliente
+  :service_id => '52430',
+  # :customer_id => 27395,
+  # :customer_ids => [27395,27396]
+  # email: { # para onde o boleto será enviado
+         # name: 'Cliente',
+         # address: 'cliente@example.com.br'
+       # }
+})
+pp bank_billet
 
 # Lista de boletos
 # bank_billets = Cobregratis::BankBillet.find(:all)
@@ -36,23 +39,23 @@ Cobregratis::Base.password = 'X'
 #   puts "=================================\n";
 # end
 
-# Criando um WebHook
-# web_hook = Cobregratis::WebHook.create({
+# Criando uma Notificação
+# notification = Cobregratis::WebHook.create({
 #   :event => 'paid', 
 #   :url => 'http://meusite.com.br/cobregratis',
 #   :code => 'codesecret'
 # })
 # 
-# pp web_hook
+# pp notification
 
-# Lista de WebHooks
-# web_hooks = Cobregratis::WebHook.find(:all)
+# Lista de notificações
+# notifications = Cobregratis::WebHook.find(:all)
 # 
-# web_hooks.each do |web_hook|   
-#   puts "ID: #{web_hook.id}\n";
-#   puts "URL: #{web_hook.url}\n";
-#   puts "Código Secreto: #{web_hook.code}\n";
-#   puts "Evento: #{web_hook.formatted_event}\n";
-#   puts "Status: #{web_hook.formatted_status}\n";
+# notifications.each do |notification|   
+#   puts "ID: #{notification.id}\n";
+#   puts "URL: #{notification.url}\n";
+#   puts "Código Secreto: #{notification.code}\n";
+#   puts "Evento: #{notification.formatted_event}\n";
+#   puts "Situação: #{notification.formatted_status}\n";
 #   puts "=================================\n";
 # end
